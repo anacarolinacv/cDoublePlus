b8ebef0e9dc3831488970d63a21c8a271b3372de
anacarolinacv <ana.vasconcelos@ccc.ufcg.edu.br>
2020-02-14 20:24:52 -0300

    oo c++

---
+++ cpp/oo/aula44.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+
+int main() {
+
+    Aviao *av1 = new Aviao("preta");
+    av1 -> defineTipoAviao(1);
+
+    cout << av1->velMax;
+
+    return 0;
+}
+
+class Aviao {
+public:
+    float velInicio = 0.0;
+    float velMax;
+    string id;
+    string tipo;
+
+    void defineTipoAviao(int id);
+
+    private:
+
+}
+
+class Aeroporto{
+public:
+    Aviao[10] avioes;
+    
+}
+
+void Aviao :: defineTipoAviao(int tp) {
+    if (tp == 1){
+        this.velMax = 800.0;
+        this.tipo = "Jato";
+    } else {
+        this.velMax = 400.0;
+        this.tipo = "Comercial ";
+    }
+    
+}
+
